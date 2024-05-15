fb00dbd974948c4fa5ae13654e68a41360f4ede4
r1p2y3 <roberta@mobikok.com>
2024-05-15 15:25:18 +0800

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,31 +1,74 @@
-import * as core from '@actions/core';
-
-export class Inputs {
-    static get path(): string | undefined {
-        const result = core.getInput('path');
-        return result === '' || result === null ? undefined : result;
-    }
-
-    static get stamp(): string | undefined {
-        const result = core.getInput('stamp');
-        return result === '' || result === null ? undefined : result;
-    }
-
-    static get setAllVars(): boolean {
-        return core.getInput('setAllVars') === 'true';
-    }
-
-    static get setCommonVars(): boolean {
-        return core.getInput('setCommonVars') === 'true';
-    }
-
-    static get toolVersion(): string | undefined {
-        const result = core.getInput('toolVersion');
-        return result === '' || result === null ? undefined : result;
-    }
-
-    static get toolFeed(): string | undefined {
-        const result = core.getInput('toolFeed');
-        return result === '' || result === null ? undefined : result;
-    }
+{
+  "app": {
+    "enablePermission": true,
+    "enableDynamicTitle": true,
+    "enableErrorLog": true,
+    "theme": "default",
+    "colorScheme": "light",
+    "elementSize": "default",
+    "defaultLang": "zh-cn",
+    "enableProgress": true,
+    "storagePrefix": "fa_",
+    "enableWatermark": false,
+    "routeBaseOn": "frontend"
+  },
+  "layout": {
+    "enableMobileAdaptation": true,
+    "widthMode": "adaption"
+  },
+  "menu": {
+    "menuFillStyle": "radius",
+    "menuActiveStyle": "dot",
+    "enableSubMenuCollapseButton": true,
+    "enableHotkeys": true,
+    "baseOn": "frontend",
+    "menuMode": "single",
+    "switchMainMenuAndPageJump": true,
+    "subMenuUniqueOpened": false,
+    "subMenuOnlyOneHide": false,
+    "subMenuCollapse": false
+  },
+  "topbar": {
+    "mode": "static",
+    "switchTabbarAndToolbar": false
+  },
+  "tabbar": {
+    "enable": false,
+    "enableIcon": true,
+    "enableHotkeys": true,
+    "style": "card",
+    "mergeTabsBy": ""
+  },
+  "toolbar": {
+    "enableNotification": true,
+    "enableI18n": true,
+    "enableFullscreen": true,
+    "enablePageReload": true,
+    "enableColorScheme": true,
+    "enableAppSetting": true
+  },
+  "breadcrumb": {
+    "style": "modern",
+    "enableMainMenu": true,
+    "enable": true
+  },
+  "mainPage": {
+    "enableHotkeys": true,
+    "iframeCacheMax": 9
+  },
+  "home": {
+    "enable": false,
+    "title": "主页"
+  },
+  "navSearch": {
+    "enable": true,
+    "enableHotkeys": true
+  },
+  "copyright": {
+    "enable": true,
+    "dates": "2020-2024",
+    "company": "OpenBI",
+    "website": "http://mobikok.openbi.com/",
+    "beian": ""
+  }
 }
